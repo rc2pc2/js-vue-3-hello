9b04978c04665a8ede48818342bdfebe0d372c2f
rc2pc2 <petriccarcc@gmail.com>
2023-12-01 10:03:17 +0100

    reactive counter examples

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            text: 'Benvenutao!',
+            counter: 0,
         }
     },
+
+    methods: {
+        incrementCounter(num){
+            this.counter = this.counter + num;
+        },
+        decrementCounter(num){
+            this.counter = this.counter - num;
+        }
+    },
+
 }).mount('#app');
